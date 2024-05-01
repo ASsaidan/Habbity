f97474701aa00c159ec00f46c5363e2725c9810e
A.saidan <35293299+Ahmadsaidan@users.noreply.github.com>
2024-05-01 20:23:56 +0200

    First commit

---
+++ Habbity/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Habbity
+//
+//  Created by A.S on 2024-05-01.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var pTitle: UILabel!
+    
+    @IBOutlet weak var statusSwitcher: UISwitch!
+    
+}
